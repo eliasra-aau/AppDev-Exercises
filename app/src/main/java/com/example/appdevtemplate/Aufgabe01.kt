197979a3d561d4995a3394445f7a3b2f95bae7e6
eliasra-aau <eliasra@edu.aau.at>
2025-04-27 08:57:38 +0200

    added red Circle

--- app/src/main/java/com/example/appdevtemplate/Aufgabe01.kt
+++ app/src/main/java/com/example/appdevtemplate/Aufgabe01.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import com.example.appdevtemplate.ui.theme.AppDevTemplateTheme
 
 class Aufgabe01 : ComponentActivity() {
@@ -27,5 +30,13 @@ class Aufgabe01 : ComponentActivity() {
 
 @Composable
 fun Screen01(modifier : Modifier = Modifier){
+    Canvas(
+        modifier = Modifier.fillMaxSize()
+    ){
 
+        val randHeight = (0..size.height.toInt()).random().toFloat()
+        val randWidth = (0..size.width.toInt()).random().toFloat()
+
+        drawCircle(color= Color.Red, center = Offset(x= randWidth, y= randHeight), radius = size.minDimension/10f)
+    }
 }
